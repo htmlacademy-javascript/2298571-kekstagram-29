5df1e34ff11528e9c8a01ab89dccb0e687e97cb6
Keks <keks@htmlacademy.ru>
2023-06-30 14:25:25 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,6 @@
+import { createPosts } from './create-posts.js';
+
+import {pictureListFragment} from './picture-preview.js';
+
+const picturePreview = document.querySelector('.pictures');
+picturePreview.append(pictureListFragment);
