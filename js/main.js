9b193ce8da7096a42638216a604d0ef717a73119
MariaRyndakova <m_i_l_o.91@mail.ru>
2023-07-21 20:38:03 +0300

    добавляет фильтрацию

--- js/main.js
+++ js/main.js
@@ -3,11 +3,15 @@ import './form-main.js';
 import './slider.js';
 import './scale.js';
 import './api.js';
+import './filters.js';
+
 import {getData, sendData} from './api.js';
 import {createThumbnails} from './picture-preview.js';
 import {sendForm, closeForm} from './form-main.js';
 import {showAlert} from './alert.js';
 import {showErrorMessage, showSuccessMessage} from './messages.js';
+import {addFilters} from './filters.js';
+import {debounce} from './util.js';
 
 const sendCallback = async(data) => {
   try{
@@ -24,6 +28,8 @@ sendForm(sendCallback);
 try {
   const data = await getData();
   createThumbnails(data);
+  const debouncedThumbnails = debounce(createThumbnails);
+  addFilters(data, debouncedThumbnails);
 } catch (err) {
   showAlert('Произошла ошибка при получении данных с сервера');
 }
--- js/util.js
+++ js/util.js
@@ -1,25 +1,44 @@
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
+// const getRandomInteger = (a, b) => {
+//   const lower = Math.ceil(Math.min(a, b));
+//   const upper = Math.floor(Math.max(a, b));
+//   const result = Math.random() * (upper - lower + 1) + lower;
+//   return Math.floor(result);
+// };
 
-function createRandomIdFromRangeGenerator (min, max) {
-  const previousValues = [];
+// function createRandomIdFromRangeGenerator (min, max) {
+//   const previousValues = [];
 
-  return function () {
-    let currentValue = getRandomInteger(min, max);
-    while (previousValues.includes(currentValue)) {
-      currentValue = getRandomInteger(min, max);
-    }
-    previousValues.push(currentValue);
-    return currentValue;
-  };
+//   return function () {
+//     let currentValue = getRandomInteger(min, max);
+//     while (previousValues.includes(currentValue)) {
+//       currentValue = getRandomInteger(min, max);
+//     }
+//     previousValues.push(currentValue);
+//     return currentValue;
+//   };
+// }
+
+// const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+function getRandomUniqueElements(array, amount) {
+  const uniqueObjects = new Set();
+  const result = [];
+
+  while (uniqueObjects.size < amount) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    uniqueObjects.add(array[randomIndex]);
+  }
+
+  uniqueObjects.forEach((element) => result.push(element));
+  return result;
 }
 
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
 
-export{getRandomInteger};
-export{createRandomIdFromRangeGenerator};
-export{getRandomArrayElement};
+export { getRandomUniqueElements, debounce};
